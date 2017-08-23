5521eaae9347403301cc1167f8c4e531f489aca4
slayerjk <slayer.jk@mail.ru>
2017-08-23 17:46:03 +0600

    Modal windows for form page(mobile) done; work on js for form.

--- app/js/script.js
+++ app/js/script.js
@@ -36,4 +36,38 @@ $(document).ready(function () {
   
   setInterval($.fn.slider2Switcher, 4000);
   */
+  
+  /*Pink-form(contest) behaviour*/
+  $('.modal-window__btn').on('click', function (e) {
+    $('.overlay').fadeOut('slow');
+    $('.modal-window').fadeOut('fast');
+  });
+  
+  function checkInput() {
+    $('.contest__input').each(function () {
+      if ($(this).prop('required') === true && $(this).val() === '') {
+        $(this).addClass('contest__input--error-js');
+        $('.contest__form-submit').addClass('error-state');
+      } else if ($(this).prop('required') === true && $(this).val() !== '') {
+        $(this).removeClass('contest__input--error-js');
+        $('.contest__form-submit').removeClass('error-state');
+      }
+    });
+  }
+  
+  $('.contest__form-submit').on('click', function (e) {
+    event.preventDefault(e);
+    $('.overlay').show();
+    checkInput();
+    if ($(this).hasClass('error-state')) {
+      $('.modal-window--error-window').fadeIn();
+    } else {
+      $('.modal-window--success-window').fadeIn();
+      $('.modal-window__btn').on('click', function () {
+        $('.contest__form').submit();
+      });
+    }
+  });
+  
+
 });
